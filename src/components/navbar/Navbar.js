217d013f6277fe965020cee2b5f3ec7c69a284f0
David Toker <david.toker@yandex.ru>
2022-08-14 03:56:34 +0300

    create navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import Badge from '@mui/material/Badge';
+
+
+// import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+
+
+const Navbar = () => {
+  const amount = useSelector((state) => state.cart.amount);
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Box sx={{ flexGrow: 2 }}>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Redux Shop
+              </Typography>
+            </Box>
+            <Box sx={{ flexGrow: 2 }} />
+            <Box
+              sx={{ flexGrow: 1, display: "flex", justifyContent: "flex-end" }}
+            >
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <Badge badgeContent={amount} color="error">
+                  <ShoppingBagIcon />
+                </Badge>
+              </IconButton>
+              <Button color="inherit">Login</Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
+}
+
+export default Navbar
