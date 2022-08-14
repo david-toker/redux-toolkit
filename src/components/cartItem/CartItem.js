5becd8e0c89ef84dc14a6801e3f1f125207b3d8d
David Toker <david.toker@yandex.ru>
2022-08-14 14:47:58 +0300

    add action to cartItem

--- src/components/cartItem/CartItem.js
+++ src/components/cartItem/CartItem.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux'
+import { removeItem, increase, decrease } from '../../redux/cart/cartSlice';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
@@ -7,6 +9,15 @@ import './cartItem.css';
 
 const CartItem = (props) => {
   const {id, img, title, price, amount } = props;
+  const dispatch = useDispatch();
+
+  const onDecrease = () => {
+    if(amount === 1) {
+      dispatch(removeItem(id));
+      return;
+    }
+    dispatch(decrease({id}))
+  }
 
   return (
     <div className="cartItem">
@@ -15,16 +26,16 @@ const CartItem = (props) => {
         <div className="cartInfo">
           <h4>{title}</h4>
           <h4>${price}</h4>
-          <Button>remove</Button>
+          <Button onClick={() => dispatch(removeItem(id))}>remove</Button>
         </div>
       </div>
 
       <div className='changeAmount'>
-        <IconButton aria-label="add" size="small">
+        <IconButton aria-label="add" size="small" onClick={() => dispatch(increase({id}))}>
           <ExpandLessIcon fontSize="inherit" />
         </IconButton>
         <p>{amount}</p>
-        <IconButton aria-label="add" size="small">
+        <IconButton aria-label="add" size="small" onClick={onDecrease}>
           <ExpandMoreIcon fontSize="inherit" />
         </IconButton>
       </div>
