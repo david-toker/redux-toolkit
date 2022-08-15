f00e9740fbb725fe429c49ccc47461802d082fe8
David Toker <david.toker@yandex.ru>
2022-08-15 03:26:41 +0300

    create modalSlice

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './redux/cart/cartSlice';
+import modalReducer from './redux/modal/modalSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   }
 })
